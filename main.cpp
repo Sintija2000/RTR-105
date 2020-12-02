a60cdc4c468533c1459308d81e6498cee021d910
Sintija2000 <71380637+Sintija2000@users.noreply.github.com>
2020-12-02 20:28:44 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int x, y, multiplication;
+    
+    cout << "Ievadiet 400000 un 100000: ";
+    cin >> x >> y;
+    multiplication = x * y;
+
+    cout << x << " * " <<  y << " = " << multiplication;     
+ 
+    return 0;
+}
